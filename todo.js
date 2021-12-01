2d63626769fc244a87bf5de0a45b2001fed10fc6
kavitatandel <kavita2810@gmail.com>
2021-12-01 15:31:00 +0100

    modified index.htm

--- todo.js
+++ todo.js
@@ -1,3 +1,18 @@
+// import './styles.css';
+
+//include css dynamically
+// var cssId = 'todo';
+// if (!document.getElementById(cssId)) {
+//     var head = document.getElementsByTagName('head')[0];
+//     var link = document.createElement('link');
+//     link.id = cssId;
+//     link.rel = 'stylesheet';
+//     link.type = 'text/css';
+//     link.href = './todo.css';
+//     link.media = 'all';
+//     head.appendChild(link);
+// }
+
 //get div with id divTodoList
 const divTodoList = document.getElementById('divTodoList');
 console.log(divTodoList);
@@ -11,19 +26,23 @@ ulTodoList.style.listStyle = 'none';
 ulTodoList.style.paddingInlineStart = '0px';
 
 //create temporary array of todoList
-let arrTodoList = ["Buy Groceries", "Go for a Walk", "Relax"];
+let arrTodoList = ["Buy Groceries", "Go for a Walk", "Buy Flowers"];
 
-//loop through arrTodoList to get items and create elements
-arrTodoList.forEach(todoElement => {
+//get the textAdd
+const inputAdd = document.getElementById('inputAdd');
+// inputAdd.innerText = inputAdd.value;
 
+function AddItems(item) {
     //create list element
     let liTodo = document.createElement('li');
     liTodo.style.margin = '10px 0px';
+
     //create radiobutton
     let radioTodo = document.createElement('input');
     radioTodo.type = 'radio';
     radioTodo.name = 'radioTodo';
-    radioTodo.value = todoElement;
+    // radioTodo.value = todoElement;
+    radioTodo.value = item;
     radioTodo.checked = false;
     radioTodo.width = '200px';
     radioTodo.height = '200px';
@@ -31,8 +50,10 @@ arrTodoList.forEach(todoElement => {
     //add radiobutton to li
     liTodo.appendChild(radioTodo);
 
+
     //get the text from array
-    let valTodo = document.createTextNode(todoElement);
+    // let valTodo = document.createTextNode(todoElement);
+    let valTodo = document.createTextNode(item);
     //append text child to li
     liTodo.appendChild(valTodo);
 
@@ -40,6 +61,7 @@ arrTodoList.forEach(todoElement => {
     let btnComplete = document.createElement('input');
     btnComplete.type = 'image';
     btnComplete.src = '/Images/complete.png';
+    //btnComplete.classList.add('buttonComplete')
     //btnComplete.class = 'buttonComplete';
     btnComplete.style.width = '20px';
     btnComplete.style.height = '20px';
@@ -74,5 +96,90 @@ arrTodoList.forEach(todoElement => {
 
     //append li to ul
     ulTodoList.appendChild(liTodo);
+}
 
-})
+//get the Add button
+const btnAdd = document.getElementById('btnAdd');
+
+btnAdd.onclick = () => {
+    AddItems(inputAdd.value)
+};
+
+
+// AddItems("My first Todo");
+// AddItems(inputAdd.value);
+
+// //loop through arrTodoList to get items and create elements
+// arrTodoList.forEach(todoElement => {
+
+//     //create list element
+//     let liTodo = document.createElement('li');
+//     liTodo.style.margin = '10px 0px';
+
+//     //create radiobutton
+//     let radioTodo = document.createElement('input');
+//     radioTodo.type = 'radio';
+//     radioTodo.name = 'radioTodo';
+//     radioTodo.value = todoElement;
+//     radioTodo.checked = false;
+//     radioTodo.width = '200px';
+//     radioTodo.height = '200px';
+//     radioTodo.style.border = "solid 1px black";
+//     //add radiobutton to li
+//     liTodo.appendChild(radioTodo);
+
+
+//     //get the text from array
+//     let valTodo = document.createTextNode(todoElement);
+//     //append text child to li
+//     liTodo.appendChild(valTodo);
+
+//     //create complete button and append to li
+//     let btnComplete = document.createElement('input');
+//     btnComplete.type = 'image';
+//     btnComplete.src = '/Images/complete.png';
+//     //btnComplete.classList.add('buttonComplete')
+//     //btnComplete.class = 'buttonComplete';
+//     btnComplete.style.width = '20px';
+//     btnComplete.style.height = '20px';
+//     btnComplete.style.marginLeft = '20px';
+//     btnComplete.style.textAlign = 'right';
+//     btnComplete.disabled = true;
+//     liTodo.appendChild(btnComplete);
+
+//     //create edit button and append to li
+//     let btnEdit = document.createElement('input');
+//     btnEdit.type = 'image';
+//     btnEdit.src = '/Images/edit.png';
+//     //btnEdit.class = 'buttonEdit';
+//     btnEdit.style.width = '30px';
+//     btnEdit.style.height = '20px';
+//     btnEdit.style.marginLeft = '20px';
+//     btnEdit.style.textAlign = 'right';
+//     btnEdit.disabled = true;
+//     liTodo.appendChild(btnEdit);
+
+//     //create delete button and append to li
+//     let btnDelete = document.createElement('input');
+//     btnDelete.type = 'image';
+//     btnDelete.src = '/Images/delete.png';
+//     //btnDelete.class = 'buttonDelete';
+//     btnDelete.style.width = '20px';
+//     btnDelete.style.height = '20px';
+//     btnDelete.style.marginLeft = '20px';
+//     btnDelete.style.textAlign = 'right';
+//     btnDelete.disabled = true;
+//     liTodo.appendChild(btnDelete);
+
+//     //append li to ul
+//     ulTodoList.appendChild(liTodo);
+
+// })
+
+// const selectedRadio = document.getElementsByName('radioTodo');
+// console.log(selectedRadio[0]);
+// // radioTodo.onchange = function (e) {
+// //     console.log(e.target);
+// // }
+// console.log(selectedRadio[1].checked)
+
